91f076c7458ffdc38d1cf442c184d6bca9503395
Alex Seymour <axeal@users.noreply.github.com>
2019-06-16 12:37:41 +0200

    Switch to traefik ingress

--- main.tf
+++ main.tf
@@ -26,72 +26,111 @@ provider "kubernetes" {
   cluster_ca_certificate = "${base64decode(digitalocean_kubernetes_cluster.k8s-cluster.kube_config.0.cluster_ca_certificate)}"
 }
 
-resource "kubernetes_service_account" "tiller" {
+resource "kubernetes_cluster_role" "traefik-ingress-controller" {
+    metadata {
+        name = "traefik-ingress-controller"
+    }
+
+    rule {
+        api_groups = [""]
+        resources  = ["services", "endpoints", "secrets"]
+        verbs      = ["get", "list", "watch"]
+    }
+
+    rule {
+        api_groups = ["extensions"]
+        resources  = ["ingresses"]
+        verbs      = ["get", "list", "watch"]
+    }
+
+    rule {
+        api_groups = ["extensions"]
+        resources  = ["ingresses/status"]
+        verbs      = ["update"]
+    }
+}
+
+resource "kubernetes_cluster_role_binding" "traefik-ingress-controller" {
+    metadata {
+        name = "traefik-ingress-controller"
+    }
+    role_ref {
+        api_group = "rbac.authorization.k8s.io"
+        kind = "ClusterRole"
+        name = "traefik-ingress-controller"
+    }
+    subject {
+        kind = "ServiceAccount"
+        namespace = "traefik"
+        name = "traefik-ingress-controller"
+    }
+}
+
+resource "kubernetes_service_account" "traefik-ingress-controller" {
   metadata {
-    name      = "tiller"
-    namespace = "kube-system"
+    name = "traefik-ingress-controller"
+    namespace = "traefik"
   }
+  automount_service_account_token = true
 }
 
-resource "kubernetes_cluster_role_binding" "tiller" {
+resource "kubernetes_daemonset" "traefik-ingress-controller" {
   metadata {
-    name      = "tiller"
-  }
-  role_ref {
-    name      = "cluster-admin"
-    kind      = "ClusterRole"
-    api_group = "rbac.authorization.k8s.io"
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = "tiller"
-    namespace = "kube-system"
-    api_group = ""
-  }
-}
-
-provider "helm" {
-  service_account = "tiller"
-  kubernetes {
-    host = "${digitalocean_kubernetes_cluster.k8s-cluster.endpoint}"
-
-    client_certificate     = "${base64decode(digitalocean_kubernetes_cluster.k8s-cluster.kube_config.0.client_certificate)}"
-    client_key             = "${base64decode(digitalocean_kubernetes_cluster.k8s-cluster.kube_config.0.client_key)}"
-    cluster_ca_certificate = "${base64decode(digitalocean_kubernetes_cluster.k8s-cluster.kube_config.0.cluster_ca_certificate)}"
-  }
-}
-
-#https://www.digitalocean.com/community/questions/hi-after-some-update-of-the-kubernetes-ltd-access-to-a-container-through-hostport-was-broken-i-assume-this-is-related-to-cilium-anyone-known-how-to-fix-that
-resource "helm_release" "nginx-ingress" {
-  name = "nginx-ingress"
-  namespace = "nginx-ingress"
-  chart = "stable/nginx-ingress"
-
-  set {
-    name = "controller.kind"
-    value = "DaemonSet"
+    name = "traefik"
+    namespace = "traefik"
+    labels = {
+      app = "traefik-ingress-controller"
+    }
   }
 
-  set {
-    name = "controller.daemonset.useHostPort"
-    value = "true"
+  spec {
+    selector {
+      match_labels = {
+        app = "traefik-ingress-controller"
+      }
+    }
+
+
+    template {
+      metadata {
+        labels = {
+          app = "traefik-ingress-controller"
+        }
+      }
+
+      spec {
+        container {
+          image = "traefik:v1.7.12"
+          name  = "traefik"
+          port {
+            name = "http"
+            container_port = 80
+            host_port = 80
+          }
+          port {
+            name = "https"
+            container_port = 443
+            host_port = 443
+          }
+          security_context {
+             capabilities {
+               drop = ["ALL"]
+               add = ["NET_BIND_SERVICE"]
+             }
+          }
+          args = [
+            "--api",
+            "--kubernetes",
+            "--logLevel=INFO",
+            "--defaultentrypoints=http,https",
+            "--entrypoints=Name:https Address::443 TLS",
+            "--entrypoints=Name:http Address::80"
+          ]
+
+        }
+        service_account_name = "traefik-ingress-controller"
+        host_network = true
+      }
+    }
   }
-
-  set {
-    name = "controller.service.type"
-    value = "ClusterIP"
-  }
-
-  set {
-    name = "controller.hostNetwork"
-    value = "true"
-  }
-
-}
-
-resource "helm_release" "cert-manager" {
-  name = "cert-manager"
-  namespace = "kube-system"
-  chart = "stable/cert-manager"
-  version = "v0.5.2"
 }
